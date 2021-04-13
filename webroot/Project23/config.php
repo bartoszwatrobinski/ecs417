1356063e5d24ac258c15b95fcc9003980bfeb0f2
bartoszwatrobinski <78392256+bartoszwatrobinski@users.noreply.github.com>
2021-04-13 14:31:01 +0000

    Update config.php

--- webroot/Project23/config.php
+++ webroot/Project23/config.php
@@ -1,12 +1,15 @@
 <?php
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'database1');
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+// Creates connection
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-if($link === false){
+if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
